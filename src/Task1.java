d904cd768fa8af66fb32224a5590919d69434157
Harnioff <harnioff@gmail.com>
2025-02-13 22:25:54 +0300

    Задание 1 : Вывести в консоль фамилию разработчика, дату и время получения задания.

---
+++ src/Task1.java
@@ -0,0 +1,7 @@
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Lagutskaya");
+        System.out.println("06.02.2025");
+        System.out.println("11:04 PM");
+    }
+}
