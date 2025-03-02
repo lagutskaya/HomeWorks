83f6c6f1c9d0fd92dec786396ff8a3a3e2ecda76
Harnioff <harnioff@gmail.com>
2025-03-02 11:51:35 +0300

    Задание 7 : Решение задачи

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,41 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import by.HomeWorks.task7.Student;
+
 public class Main {
-    public static void main (String [] args) {
+    public static void main(String[] args) {
         System.out.println("Lagutskaya");
+
+
+        Student student1 = new Student(1, "Иван", "Иванов", "Инженер", 2, "201", 85);
+        Student student2 = new Student();
+
+        student1.info();
+
+        student2.info();
+
+
+        student1.changeGroup("201/2");
+        //System.out.println("После изменения группы : ");
+        //student1.info();
+
+        String currentGroup = student1.getCurrentGroup();
+        //  System.out.println("Новая группа " + currentGroup);
+
+        student1.changeRatingAndGroup(100, "I-212");
+        //System.out.println("После изменения оценки и группы:");
+        // student1.info();
+
+        Student[] students = new Student[5];
+        students[0] = new Student(1, "Костя", "Сидоров", "Инженер", 2, "101", 100);
+        students[1] = new Student(2, "Андрей", "Петров", "Певец", 3, "102", 90);
+        students[2] = new Student(3, "Катя", "Котова", "Тестировщик", 1, "103", 75);
+        students[3] = new Student(4, "Николай", "Потемкин", "Клинер", 2, "104", 43);
+        students[4] = new Student(5, "Сан", "Саныч", "Юрист", 3, "105", 80);
+
+        Student[] highRatingStudents = Student.getStudents(students, 80);
+
+        System.out.println("Студенты с оценкой выше 80 : ");
+        for (Student student : highRatingStudents) {
+            student.info();
+        }
     }
 }
